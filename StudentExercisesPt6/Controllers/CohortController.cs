db9328978207f8d687b6d2fc6e7213ccc4c890e6
Joseph Snyder <snyderjosephd@yahoo.com>
2019-11-01 08:39:54 -0500

    Stopped duplication of instructors and students when ?q= parameter is used on CohortController

--- StudentExercisesPt6/Controllers/CohortController.cs
+++ StudentExercisesPt6/Controllers/CohortController.cs
@@ -29,10 +29,106 @@ namespace StudentExercisesPt6.Controllers
             }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            if (q != null)
+            {
+                // q parameter is used/passed in
+                return await GetCohortsWithQ(q);
+            }
+            else
+            {
+                // q parameter is not used
+                return await GetCohorts();
+            }
+        }
+
+        // Get cohorts with use of q query string parameter
+        private async Task<IActionResult> GetCohortsWithQ(string q)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id as 'TheCohortId', c.Name as 'CohortName',
+		                                        s.Id as 'TheStudentId', s.FirstName as 'StudentFirstName', s.LastName as                            'StudentLastName', s.SlackHandle as 'StudentSlackHandle',
+		                                        i.Id as 'TheInstructorId', i.FirstName as 'InstructorFirstName', i.LastName as                      'InstructorLastName', i.SlackHandle as 'InstructorSlackHandle', i.Specialty
+                                                FROM Cohort c LEFT JOIN Student s ON s.CohortId = c.Id LEFT JOIN Instructor i ON                    i.CohortId = c.Id
+                                                WHERE c.Name LIKE @q";
+                    cmd.Parameters.Add(new SqlParameter("@q", q));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Dictionary<int, Cohort> cohorts = new Dictionary<int, Cohort>();
+                    while (reader.Read())
+                    {
+                        int cohortId = reader.GetInt32(reader.GetOrdinal("TheCohortId"));
+                        if (!cohorts.ContainsKey(cohortId))
+                        {
+                            Cohort cohort = new Cohort
+                            {
+                                Id = cohortId,
+                                Name = reader.GetString(reader.GetOrdinal("CohortName")),
+                                Students = new List<Student>(),
+                                Instructors = new List<Instructor>()
+                            };
+
+                            cohorts.Add(cohortId, cohort);
+                        }
+
+                        Cohort fromDictionary = cohorts[cohortId];
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("TheStudentId")))
+                        {
+                            int studentId = reader.GetInt32(reader.GetOrdinal("TheStudentId"));
+
+                            if (! cohorts[cohortId].Students.Any(s => s.Id == studentId))
+                            {
+                                Student student = new Student
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("TheStudentId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("StudentFirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("StudentLastName")),
+                                    SlackHandle = reader.GetString(reader.GetOrdinal("StudentSlackHandle")),
+                                    CohortId = reader.GetInt32(reader.GetOrdinal("TheCohortId")),
+                                    Cohort = null,
+                                    Exercises = new List<Exercise>()
+                                };
+                                fromDictionary.Students.Add(student);
+                            }
+                        }
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("TheInstructorId")))
+                        {
+                            int instructorId = reader.GetInt32(reader.GetOrdinal("TheInstructorId"));
+
+                            if (!cohorts[cohortId].Instructors.Any(i => i.Id == instructorId))
+                            {
+                                Instructor instructor = new Instructor
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("TheInstructorId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("InstructorFirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("InstructorLastName")),
+                                    SlackHandle = reader.GetString(reader.GetOrdinal("InstructorSlackHandle")),
+                                    Specialty = reader.GetString(reader.GetOrdinal("Specialty")),
+                                    CohortId = reader.GetInt32(reader.GetOrdinal("TheCohortId")),
+                                    Cohort = null
+                                };
+                                fromDictionary.Instructors.Add(instructor);
+                            }
+                        }
+                    }
+                    reader.Close();
+
+                    return Ok(cohorts.Values);
+                }
+            }
+        }
+
         ///<summary>Gets all the cohorts from the database</summary>
         // GET: api/cohort
-        [HttpGet]
-        public async Task<IActionResult> GetAllCohorts()
+        private async Task<IActionResult> GetCohorts()
         {
             using (SqlConnection conn = Connection)
             {
